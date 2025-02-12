ea548a2a465b714d53d37f2440bcdd670e21db74
cyrus <cyrus28214@gmail.com>
2025-02-12 16:39:55 +0800

    feat(log): 添加时间

--- internal/log/log.go
+++ internal/log/log.go
@@ -2,6 +2,7 @@ package log
 
 import (
 	"os"
+	"time"
 
 	"github.com/rs/zerolog"
 )
@@ -20,15 +21,26 @@ func init() {
 		}
 	}
 
+	// 设置全局时区为本地时区
+	zerolog.TimeFieldFormat = time.RFC3339
+
 	logPath := os.Getenv("LOG_PATH")
 	if logPath == "" {
-		Logger = zerolog.New(os.Stderr).Level(logLevel)
+		Logger = zerolog.New(os.Stderr).
+			Level(logLevel).
+			With().
+			Timestamp(). // 添加时间戳
+			Logger()
 	} else {
 		file, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 		if err != nil {
 			panic(err)
 		}
-		Logger = zerolog.New(file).Level(logLevel)
+		Logger = zerolog.New(file).
+			Level(logLevel).
+			With().
+			Timestamp(). // 添加时间戳
+			Logger()
 	}
 
 	Logger.Info().Msg("Log level: " + logLevel.String())
